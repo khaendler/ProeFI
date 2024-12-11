80024b8e5b8ff2090fe1d6c8656bc879fec87a9d
khaendle <khaendle@students.uni-mainz.de>
2024-12-11 11:44:52 +0100

    added comments

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from data.datasets.experiment_datasets import *
 from utils.io_helpers import save
 
 
+# Evaluates each model on a chosen dataset. Collects number of nodes, accuracy values, kappa scores and
+# feature importance values (if available) for each instance.
 if __name__ == '__main__':
 
     models = [("ht", HoeffdingTreeClassifier()),
@@ -33,9 +35,12 @@ if __name__ == '__main__':
               ("adwin_hpt_merit_seed42", AdwinHPTMerit(seed=42))
               ]
 
-    datasets = [("airlines", lambda: Airlines()), ("electricity", lambda: Elec2()),
-                ("kdd99", lambda: KDD99()), ("wisdm", lambda: WISDM()),
-                ("covtype", lambda: CovType()), ("nomao", lambda: Nomao()),
+    datasets = [("airlines", lambda: Airlines()),
+                ("electricity", lambda: Elec2()),
+                ("kdd99", lambda: KDD99()),
+                ("wisdm", lambda: WISDM()),
+                ("covtype", lambda: CovType()),
+                ("nomao", lambda: Nomao()),
                 ("agr_a", lambda: DriftingAgrawal(width=50).take(10**6)),
                 ("agr_g", lambda: DriftingAgrawal(width=50000).take(10**6)),
                 ("rbf_f", lambda: DriftingRBF(change_speed=0.001).take(10**6)),
@@ -44,7 +49,7 @@ if __name__ == '__main__':
                 ("led_g", lambda: DriftingLED(width=50000).take(10**6))
                 ]
 
-    data_name, data_generator = datasets[0]  # choose the dataset
+    data_name, data_generator = datasets[0]  # choose the dataset, 0-11
     for (model_name, model) in models:
         data = data_generator()
 
@@ -59,7 +64,7 @@ if __name__ == '__main__':
         for i, (x, y) in enumerate(data, start=1):
 
             y_pred = model.predict_one(x)
-
+            # Updates metrics and stores the values.
             accuracy.update(y, y_pred)
             acc_values.append(accuracy.get())
             kappa.update(y, y_pred)
@@ -76,22 +81,23 @@ if __name__ == '__main__':
                     "------------------------------------------------------------"
                 )
 
-        avg_total_acc = np.mean(acc_values)
+        # Stores collected stats.
         save(acc_values, f"results/acc_values/{model_name}_{data_name}.npy")
-
-        avg_total_auc = np.mean(kappa_values)
         save(kappa_values, f"results/kappa_values/{model_name}_{data_name}.npy")
-
-        avg_total_nodes = np.mean(n_nodes)
         save(n_nodes, f"results/n_nodes/{model_name}_{data_name}.npy")
 
+        # Stores feature importance values if the model collected any.
         if isinstance(model, AdwinHPT):
             save(model.collected_importance_values, f"results/fi_values/{model_name}_{data_name}.npy")
+
+        avg_total_acc = np.mean(acc_values)
+        avg_total_kappa = np.mean(kappa_values)
+        avg_total_nodes = np.mean(n_nodes)
 
         print(
             f"Summary for {model_name} on {data_name}:\n"
             f"  - Total avg. node count: {avg_total_nodes}\n"
-            f"  - Avg. AUC: {avg_total_auc:.3f}\n"
+            f"  - Avg. AUC: {avg_total_kappa:.3f}\n"
             f"  - Avg. Accuracy: {avg_total_acc:.3f}\n"
             "------------------------------------------------------------"
         )
