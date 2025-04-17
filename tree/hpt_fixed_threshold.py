a57cf4afe6fcb2031242ea4362473828afa53bd3
khaendle <khaendle@students.uni-mainz.de>
2025-04-17 12:46:07 +0200

    Implemented HPTFixedThreshold

---
+++ tree/hpt_fixed_threshold.py
@@ -0,0 +1,115 @@
+from river.tree.splitter import Splitter
+
+from tree import HoeffdingPruningTree
+
+
+class HPTFixedThreshold(HoeffdingPruningTree):
+    """Hoeffding Pruning Tree using a fixed importance threshold.
+
+    Parameters
+    ----------
+    grace_period
+        Number of instances a leaf should observe between split attempts.
+    max_depth
+        The maximum depth a tree can reach. If `None`, the tree will grow indefinitely.
+    split_criterion
+        Split criterion to use.</br>
+        - 'gini' - Gini</br>
+        - 'info_gain' - Information Gain</br>
+        - 'hellinger' - Helinger Distance</br>
+    delta
+        Significance level to calculate the Hoeffding bound. The significance level is given by
+        `1 - delta`. Values closer to zero imply longer split decision delays.
+    tau
+        Threshold below which a split will be forced to break ties.
+    leaf_prediction
+        Prediction mechanism used at leafs.</br>
+        - 'mc' - Majority Class</br>
+        - 'nb' - Naive Bayes</br>
+        - 'nba' - Naive Bayes Adaptive</br>
+    nb_threshold
+        Number of instances a leaf should observe before allowing Naive Bayes.
+    nominal_attributes
+        List of Nominal attributes identifiers. If empty, then assume that all numeric
+        attributes should be treated as continuous.
+    splitter
+        The Splitter or Attribute Observer (AO) used to monitor the class statistics of numeric
+        features and perform splits. Splitters are available in the `tree.splitter` module.
+        Different splitters are available for classification and regression tasks. Classification
+        and regression splitters can be distinguished by their property `is_target_class`.
+        This is an advanced option. Special care must be taken when choosing different splitters.
+        By default, `tree.splitter.GaussianSplitter` is used if `splitter` is `None`.
+    binary_split
+        If True, only allow binary splits.
+    max_size
+        The max size of the tree, in Megabytes (MB).
+    memory_estimate_period
+        Interval (number of processed instances) between memory consumption checks.
+    stop_mem_management
+        If True, stop growing as soon as memory limit is hit.
+    remove_poor_attrs
+        If True, disable poor attributes to reduce memory usage.
+    merit_preprune
+        If True, enable merit-based tree pre-pruning.
+    pruner
+        The Pruner to use.</br>
+        - 'selective' - SelectivePruner</br>
+        - 'complete' - CompletePruner</br>
+    seed
+        Random seed for reproducibility.
+    importance_threshold
+        A fixed importance threshold to prune the tree based on feature importance.
+    """
+
+    def __init__(
+        self,
+        grace_period: int = 200,
+        max_depth: int = None,
+        split_criterion: str = "info_gain",
+        delta: float = 1e-7,
+        tau: float = 0.05,
+        leaf_prediction: str = "nba",
+        nb_threshold: int = 0,
+        nominal_attributes: list = None,
+        splitter: Splitter = None,
+        binary_split: bool = False,
+        min_branch_fraction: float = 0.01,
+        max_share_to_split: float = 0.99,
+        max_size: float = 100.0,
+        memory_estimate_period: int = 1000000,
+        stop_mem_management: bool = False,
+        remove_poor_attrs: bool = False,
+        merit_preprune: bool = True,
+        pruner: str = "complete",
+        seed: int = None,
+        importance_threshold: float = 0.02
+    ):
+
+        super().__init__(
+            grace_period=grace_period,
+            max_depth=max_depth,
+            split_criterion=split_criterion,
+            delta=delta,
+            tau=tau,
+            leaf_prediction=leaf_prediction,
+            nb_threshold=nb_threshold,
+            nominal_attributes=nominal_attributes,
+            splitter=splitter,
+            binary_split=binary_split,
+            min_branch_fraction=min_branch_fraction,
+            max_share_to_split=max_share_to_split,
+            max_size=max_size,
+            memory_estimate_period=memory_estimate_period,
+            stop_mem_management=stop_mem_management,
+            remove_poor_attrs=remove_poor_attrs,
+            merit_preprune=merit_preprune,
+            pruner=pruner,
+            seed=seed
+        )
+
+        self.importance_threshold = importance_threshold
+
+    def _update_importance_threshold(self):
+        pass
+
+
