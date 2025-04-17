0139407b4b1aacc711db29ceff6677023aa6376c
khaendle <khaendle@students.uni-mainz.de>
2025-04-17 12:48:35 +0200

    Implemented HPTFixedThreshold

--- tree/__init__.py
+++ tree/__init__.py
@@ -2,6 +2,7 @@ from .hoeffding_pruning_tree import HoeffdingPruningTree
 from .hpt_convex_merit import HPTConvexMerit
 from .hpt_merit import HPTMerit
 from .ht_merit import HTMerit
+from .hpt_fixed_threshold import HPTFixedThreshold
 
 
-__all__ = ["HoeffdingPruningTree", "HPTConvexMerit", "HPTMerit", "HTMerit"]
+__all__ = ["HoeffdingPruningTree", "HPTConvexMerit", "HPTMerit", "HTMerit", "HPTFixedThreshold"]
